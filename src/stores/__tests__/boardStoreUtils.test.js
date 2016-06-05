3e3477ea8a507693e19158f3dfe2026c0c0e75ae
yzuniversal <yzukonta@gmail.com>
2016-06-05 22:00:22 +0200

    Refactoring boardStoreUtils and writing tests for it

--- src/stores/__tests__/boardStoreUtils.test.js
+++ src/stores/__tests__/boardStoreUtils.test.js
@@ -1,5 +1,6 @@
 jest.unmock('@stores/boardStoreUtils');
 jest.unmock('random-js');
+jest.unmock('immutable');
 
 const Immutable = require('immutable');
 const boardStoreUtils = require('@stores/boardStoreUtils');
@@ -8,11 +9,44 @@ describe('A set of utility functions for the BoardStore', () => {
 
   describe('A function which generates a board layout together with a set of answers for it', () => {
 
-    it('should create a new Immutable object containing a boardLayout property', () => {
+    it('should create a new Immutable object containing a boardLayout, answersLength2 and answersLength3 properties', () => {
       const result = boardStoreUtils.createNewBoard();
 
       expect(Immutable.Map.isMap(result)).toBe(true);
       expect(result.get('boardLayout')).toBeDefined();
+      expect(result.get('answersLength2')).toBeDefined();
+      expect(result.get('answersLength3')).toBeDefined();
+    });
+
+    it('should contain a boardLayout property, which is an Immutable.List containing 9 values; values with indices of 1, 3, 5, 7 should be strings containing either "+" or "-", while the rest should be integers', () => {
+      const result = boardStoreUtils.createNewBoard();
+      const layout = result.get('boardLayout');
+      const operators = layout.filter((value, key) => {
+        return key === 1 || key === 3 || key === 5 || key === 7;
+      });
+      const numbers = layout.filter((value, key) => {
+        return key !== 1 && key !== 3 && key !== 5 && key !== 7;
+      });
+
+      expect(Immutable.List.isList(layout)).toBe(true);
+      expect(layout.size).toBe(9);
+      expect(operators.every(value => value === '+' || value === '-')).toBe(true);
+      expect(numbers.every(value => Number.isInteger(value))).toBe(true);
+    });
+
+    it('should return a new, random boardLayout every time it is called', () => {
+      const result1 = boardStoreUtils.createNewBoard().get('boardLayout');
+      const result2 = boardStoreUtils.createNewBoard().get('boardLayout');
+
+      expect(result1.equals(result2)).toBe(false);
+    });
+
+    it('should accept a numerical seed value, and ensure that every boardLayout generated with the same seed is the same', () => {
+      const seed = 123456;
+      const result1 = boardStoreUtils.createNewBoard(seed).get('boardLayout');
+      const result2 = boardStoreUtils.createNewBoard(seed).get('boardLayout');
+
+      expect(result1.equals(result2)).toBe(true);
     });
 
   });
--- src/stores/boardStoreUtils.js
+++ src/stores/boardStoreUtils.js
@@ -1,53 +1,238 @@
+// const Immutable = require('immutable');
+// const Random = require('random-js');
+//
+//
+// // Polyfill:
+// Number.isInteger = Number.isInteger || function isInteger(value) {
+//   return typeof value === 'number' &&
+//     isFinite(value) &&
+//     Math.floor(value) === value;
+// };
+//
+//
+// /*
+//   Board layout is:
+//     0 1 2
+//     3 4 5
+//     6 7 8
+//   where id's 1, 3, 5, 7 are operators, and rest are numbers.
+// */
+//
+// function isOperator(value) {
+//   if (value === '+' || value === '-') {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+//
+// function isNumber(value) {
+//   return Number.isInteger(value);
+// }
+//
+// function generateBoardLayout(seed) {
+//   let r = new Random();
+//
+//   if (seed) {
+//     r = r.seed(seed);
+//   }
+//
+//   const operators = ['+', '-', '-', '+'];
+//   const numbers = (() => {
+//     const result = [];
+//
+//     for (let i = 0; i < 5; i++) {
+//       result.push(r.integer(0, 9));
+//     }
+//
+//     return result;
+//   })();
+//
+//   return Immutable.fromJS([
+//     numbers.shift(),
+//     operators.shift(),
+//     numbers.shift(),
+//
+//     operators.shift(),
+//     numbers.shift(),
+//     operators.shift(),
+//
+//     numbers.shift(),
+//     operators.shift(),
+//     numbers.shift(),
+//   ]);
+// }
+//
+// function generateAnswers({boardLayout, length, seed, numberOfAnswers}) {
+//   let r = new Random();
+//
+//   if (seed) {
+//     r = r.seed(seed);
+//   }
+//
+//   const answers = [];
+//   let i = 0;
+//
+//   while (i < numberOfAnswers) {
+//     const answer = generateOneAnswer();
+//
+//     if (answer.sum < 1) {
+//       continue;
+//     }
+//
+//     if (answers.find(a => a.sum === answer.sum)) {
+//       continue;
+//     }
+//
+//     answers.push(answer);
+//
+//     i++;
+//   }
+//
+//   return Immutable.fromJS(answers);
+//
+//   function generateOneAnswer() {
+//     const path = [];
+//     const startingPoint = r.pick([0, 2, 4, 6, 8]);
+//
+//     path.push(startingPoint);
+//
+//     let i = 0;
+//
+//     while (i < length - 1) {
+//       //  Equals: move left, move right, move up, move left:
+//       const move = r.pick([-1, 1, -3, 3]);
+//       const currentPosition = path[path.lenght - 1];
+//       const nextPosition = currentPosition + move;
+//       const currentValue = boardLayout.get(currentPosition);
+//       const nextValue = boardLayout.get(nextPosition);
+//
+//       if (path.find(id => id === nextPosition)) {
+//         continue;
+//       }
+//
+//       if (!boardLayout.get(nextPosition)) {
+//         continue;
+//       }
+//
+//       if (
+//         (isNumber(currentValue) && isNumber(nextValue)) ||
+//         (isOperator(currentValue) && isOperator(nextValue))
+//       ) {
+//         continue;
+//       }
+//
+//       path.push(nextValue);
+//
+//       i++;
+//     }
+//
+//     return {
+//       path,
+//       sum: makeSumForAnswer(path),
+//     };
+//   }
+//
+//   function makeSumForAnswer(path) {
+//     path = path.slice();
+//
+//     const length = path.length;
+//     let sum = 0;
+//     let num1;
+//     let num2;
+//     let operator;
+//
+//     for (let i = 0; i < length - 1; i++) {
+//       if (!num1) {
+//         num1 = path.shift();
+//
+//         continue;
+//       }
+//       else if (!operator) {
+//         operator = path.shift();
+//
+//         continue;
+//       }
+//       else if (!num2) {
+//         num2 = path.shift();
+//
+//         if (operator === '+') {
+//           sum += add(num1, num2);
+//         }
+//         else if (operator === '-') {
+//           sum += subtract(num1, num2);
+//         }
+//
+//         num1 = sum;
+//
+//         num2 = operator = undefined;
+//       }
+//     }
+//
+//     return sum;
+//
+//     function add(num1, num2) {
+//       return num1 + num2;
+//     }
+//
+//     function subtract(num1, num2) {
+//       return num1 - num2;
+//     }
+//   }
+// }
+//
+// function createNewBoard(seed) {
+//   const boardLayout = generateBoardLayout(seed);
+//   const answersLength2 = generateAnswers({
+//     boardLayout,
+//     length: 2,
+//     numberOfAnswers: 3,
+//     seed,
+//   });
+//
+//   return Immutable.fromJS({
+//     boardLayout,
+//     answersLength2,
+//   });
+// }
+//
+//
+// module.exports = {
+//   createNewBoard,
+// };
+
 const Immutable = require('immutable');
 const Random = require('random-js');
 
 
-// Polyfill:
-Number.isInteger = Number.isInteger || function isInteger(value) {
-  return typeof value === 'number' &&
-    isFinite(value) &&
-    Math.floor(value) === value;
-};
-
-
 /*
-  Board layout is:
+  The board has a shape of a 3x3 square. A correctly generated board layout should be an Immutable.List whose indices will be laid out as follows:
     0 1 2
     3 4 5
     6 7 8
-  where id's 1, 3, 5, 7 are operators, and rest are numbers.
+
+  This board is composed of 4 operators and 5 numbers, looking as such (n = number, o = operator):
+    n o n
+    o n o
+    n o n
+
+  Right now the operators can only be 'plus' and 'minus', and they're position on the board can only be as follows:
+    n + n
+    - n -
+    n + n
 */
 
-function isOperator(value) {
-  if (value === '+' || value === '-') {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
 
-function isNumber(value) {
-  return Number.isInteger(value);
-}
-
-function generateBoardLayout(seed) {
-  let r = new Random();
-
-  if (seed) {
-    r = r.seed(seed);
-  }
+function generateBoardLayout(random) {
+  const r = random || new Random();
 
   const operators = ['+', '-', '-', '+'];
-  const numbers = (() => {
-    const result = [];
+  const numbers = [];
 
-    for (let i = 0; i < 5; i++) {
-      result.push(r.integer(0, 9));
-    }
-
-    return result;
-  })();
+  for (let i = 0; i < 5; i++) {
+    numbers.push(r.integer(0, 9));
+  }
 
   return Immutable.fromJS([
     numbers.shift(),
@@ -64,140 +249,29 @@ function generateBoardLayout(seed) {
   ]);
 }
 
-function generateAnswers({boardLayout, length, seed, numberOfAnswers}) {
-  let r = new Random();
+function createNewBoard(seed) {
+  let random;
 
   if (seed) {
-    r = r.seed(seed);
+    random = new Random(Random.engines.mt19937().seed(seed));
   }
 
-  const answers = [];
-  let i = 0;
+  const boardLayout = generateBoardLayout(random);
+  const answersLength2 = {};
+  const answersLength3 = {};
 
-  while (i < numberOfAnswers) {
-    const answer = generateOneAnswer();
-
-    if (answer.sum < 1) {
-      continue;
-    }
-
-    if (answers.find(a => a.sum === answer.sum)) {
-      continue;
-    }
-
-    answers.push(answer);
-
-    i++;
-  }
-
-  return Immutable.fromJS(answers);
-
-  function generateOneAnswer() {
-    const path = [];
-    const startingPoint = r.pick([0, 2, 4, 6, 8]);
-
-    path.push(startingPoint);
-
-    let i = 0;
-
-    while (i < length - 1) {
-      //  Equals: move left, move right, move up, move left:
-      const move = r.pick([-1, 1, -3, 3]);
-      const currentPosition = path[path.lenght - 1];
-      const nextPosition = currentPosition + move;
-      const currentValue = boardLayout.get(currentPosition);
-      const nextValue = boardLayout.get(nextPosition);
-
-      if (path.find(id => id === nextPosition)) {
-        continue;
-      }
-
-      if (!boardLayout.get(nextPosition)) {
-        continue;
-      }
-
-      if (
-        (isNumber(currentValue) && isNumber(nextValue)) ||
-        (isOperator(currentValue) && isOperator(nextValue))
-      ) {
-        continue;
-      }
-
-      path.push(nextValue);
-
-      i++;
-    }
-
-    return {
-      path,
-      sum: makeSumForAnswer(path),
-    };
-  }
-
-  function makeSumForAnswer(path) {
-    path = path.slice();
-
-    const length = path.length;
-    let sum = 0;
-    let num1;
-    let num2;
-    let operator;
-
-    for (let i = 0; i < length - 1; i++) {
-      if (!num1) {
-        num1 = path.shift();
-
-        continue;
-      }
-      else if (!operator) {
-        operator = path.shift();
-
-        continue;
-      }
-      else if (!num2) {
-        num2 = path.shift();
-
-        if (operator === '+') {
-          sum += add(num1, num2);
-        }
-        else if (operator === '-') {
-          sum += subtract(num1, num2);
-        }
-
-        num1 = sum;
-
-        num2 = operator = undefined;
-      }
-    }
-
-    return sum;
-
-    function add(num1, num2) {
-      return num1 + num2;
-    }
-
-    function subtract(num1, num2) {
-      return num1 - num2;
-    }
-  }
-}
-
-function createNewBoard(seed) {
-  const boardLayout = generateBoardLayout(seed);
-  const answersLength2 = generateAnswers({
-    boardLayout,
-    length: 2,
-    numberOfAnswers: 3,
-    seed,
-  });
 
   return Immutable.fromJS({
     boardLayout,
     answersLength2,
+    answersLength3,
   });
 }
 
 
 module.exports = {
   createNewBoard,
+  __testing: {
+    generateBoardLayout,
+  },
 };
